5207fe0034058baedd51340cb1fd4afcd2ca288a
PittiglioFrancesco <francescopittiglio79@gmail.com>
2021-09-05 18:40:02 +0200

    fine

--- laravel-primi-passi/routes/web.php
+++ laravel-primi-passi/routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        'uno' => 'Primo elemento',
+        'due' => 'Secondo elemento',
+        'tre' => 'Terzo elemento'
+    ];
+    return view('home', $data);
 });
